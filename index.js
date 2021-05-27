769a7443478de81c5def30d2aef752697f71ae28
JacobLinCool <jacoblincool@gmail.com>
2021-05-27 10:50:19 +0800

    improve cache strategy

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import { get_data } from "./src/api.js";
 import { handle_cron } from "./src/cron.js";
-import { handle_websocket } from "./src/websocket.js";
 
 addEventListener("fetch", (event) => {
     event.respondWith(handle_request(event));
@@ -11,31 +10,26 @@ addEventListener("scheduled", (event) => {
 });
 
 async function handle_request(event) {
-    const request = event.request;
+    const url = "https://taipower-api.jacob.workers.dev/";
+    const request = new Request(url);
 
-    const upgrade_header = request.headers.get("Upgrade");
-    if (upgrade_header && upgrade_header === "websocket") {
-        return handle_websocket(request);
-    } else {
-        const url = new URL(request.url).origin + "/";
-        const cache_key = new Request(url, request);
-        const cache = caches.default;
+    const cache_key = new Request(url, request);
+    const cache = caches.default;
 
-        let response = await cache.match(cache_key);
-        if (!response) {
-            const processed_data = await get_data();
+    let response = await cache.match(cache_key);
+    if (!response) {
+        const processed_data = await get_data();
 
-            response = new Response(JSON.stringify(processed_data, null, 4), {
-                headers: {
-                    "Content-Type": "application/json",
-                    "Cross-Origin-Resource-Policy": "cross-origin",
-                    "Access-Control-Allow-Origin": "*",
-                    "Access-Control-Allow-Credentials": "true",
-                },
-            });
-            response.headers.append("Cache-Control", "s-maxage=30");
-            event.waitUntil(cache.put(cache_key, response.clone()));
-        }
-        return response;
+        response = new Response(JSON.stringify(processed_data, null, 4), {
+            headers: {
+                "Content-Type": "application/json",
+                "Cross-Origin-Resource-Policy": "cross-origin",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Credentials": "true",
+            },
+        });
+        response.headers.append("Cache-Control", "max-age=45, s-maxage=45");
+        event.waitUntil(cache.put(cache_key, response.clone()));
     }
+    return response;
 }
--- src/cron.js
+++ src/cron.js
@@ -1,5 +1,27 @@
 import { get_data, get_data_from_taipower_api, process_taipower_api_data } from "./api.js";
 
-async function handle_cron(event) {}
+async function handle_cron(event) {
+    const url = "https://taipower-api.jacob.workers.dev/";
+    const request = new Request(url);
+
+    const cache_key = new Request(url, request);
+    const cache = caches.default;
+
+    let response = await cache.match(cache_key);
+    if (!response) {
+        const processed_data = await get_data();
+
+        response = new Response(JSON.stringify(processed_data, null, 4), {
+            headers: {
+                "Content-Type": "application/json",
+                "Cross-Origin-Resource-Policy": "cross-origin",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Credentials": "true",
+            },
+        });
+        response.headers.append("Cache-Control", "max-age=45, s-maxage=45");
+        await cache.put(cache_key, response.clone());
+    }
+}
 
 export { handle_cron };
